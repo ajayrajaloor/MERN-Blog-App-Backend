ef59315c8282fbdbc0949c331f540789ecd074f4
ajayraj1111 <121021527+ajayraj1111@users.noreply.github.com>
2024-01-21 23:03:31 +0530

    already pushed project. Backend pushing for deploy

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+const verifyToken = (req,res,next)=>{
+
+    if(!req.headers.authorization) return res.status(403).json({msg:"Not autorized. No token"})
+
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer ")){
+        const token = req.headers.authorization.split(' ')[1]
+        
+        jwt.verify(token, process.env.JWT_SECRET, (err, data)=>{
+            if(err) return res.status(403).json({msg: "Wrong or expired token"})
+            else{
+                req.user = data //an object with the user id as its only property
+                next()
+            }
+        })
+    }
+}
+
+module.exports = verifyToken;
